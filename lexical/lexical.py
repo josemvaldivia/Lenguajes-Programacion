acbfd0598efb71abc408326b3a5395b00db102f1
José <josevaldiviaromero@gmail.com>
2015-09-01 09:59:12 -0500

    Lexical Interpretation

---
+++ lexical/lexical.py
@@ -0,0 +1,20 @@
+
+symbols = {	'(': 'left_parent',
+			'(': 'right_parent',
+			'+': 'sum',
+			'-': 'minus',
+			'*': 'mult',
+			'/': 'div'
+		}
+
+test_data = '(sum + 47) / total'
+def getData(text):
+	for ĺetter in list(text):
+		
+
+
+
+
+
+if __name__ == '__main__':
+	main()
